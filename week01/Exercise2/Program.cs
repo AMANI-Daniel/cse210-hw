86a95b86fea1a0f0930bbfd697b42c865fac16da
AMANI-Daniel <adaniel8@byupathway.edu>
2026-05-08 14:00:39 +0200

    I have completed the exerciese 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,58 @@
 using System;
+using System.Data.Common;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is Grade in percentage? ");
+        String userGrade = Console.ReadLine();
+        int grades = int.Parse(userGrade);
+        string letter = "";
+        int remainder = grades % 10;
+
+        string sign = "";
+        if (remainder >= 7 || grades !< 94)
+        {
+            sign = "+";
+        }
+        if (remainder < 3)
+        {
+            sign = "-";
+        }
+
+
+        if (grades >= 90)
+        {
+            letter = "A" + sign;
+        }
+        else if (grades >= 80)
+        {
+            letter = "B" + sign;
+        }
+        else if (grades >= 70)
+        {
+            letter = "C" + sign;
+        }
+        else if (grades >= 60)
+        {
+            letter = "D" + sign;
+        }
+        else
+        {
+            letter = "F";
+        }
+        if (grades >= 70)
+        {
+            Console.WriteLine("Conguratulations!!! You've passed.");
+        }
+        else
+        {
+            Console.WriteLine("You have failes, but keep working hard to pass in th future.");
+        }
+
+        Console.WriteLine($"Your Grades letter is {letter}");
+
     }
 }
