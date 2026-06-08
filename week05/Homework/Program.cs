fe1493ddd6df2d52f27a2a7ed463fa9b3c97755d
AMANI-Daniel <adaniel8@byupathway.edu>
2026-06-08 12:35:15 +0200

    I completed the homework project

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        //Creating an instance assignment class
+
+        Assignment assignment = new Assignment("Daniel", "Programming");
+
+        string summary = assignment.GetSummary();
+
+        Console.WriteLine(summary);
+        Console.WriteLine("----------------------------------------");
+
+
+        MathAssignment mathAssignment = new MathAssignment("Amani", "Fractions", "Section 7.3", "Problems 9-18");
+    
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+
+        Console.WriteLine("----------------------------------------");
+
+        WritingAssignment writingAssignment = new WritingAssignment("Rose", "European History", "The Causes of World War II");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
+
+
     }
 }
