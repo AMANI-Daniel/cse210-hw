300278c53dace89a063fee9eb0e9cb5968ae5f38
AMANI-Daniel <adaniel8@byupathway.edu>
2026-06-11 20:07:43 +0200

    I completed the shape activity.

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        // Console.WriteLine("Hello World! This is the Shapes Project.");
+
+        Square square = new Square("red", 6);
+        Rectangle rectangle = new Rectangle("blue", 4, 5);
+        Circle circle = new Circle("green", 4);
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        foreach(Shape s in shapes)
+        {
+            string color = s.GetColor();
+            double area = s.GetArea();
+            Console.WriteLine($"The shape is {color} and its area is {area}.");
+        }
+
     }
 }
